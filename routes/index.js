52c515e0931e6fd8b623f12c68ed71d5ab46b0e0
Geonhwi Lee <bryanlee@blockware.co.kr>
2023-09-23 15:50:53 +0900

    4주차 수업 버그 수정

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // render: 페이지 출력
-  // res.render('index', { title: '익스프레스', name: '홍길동' });
-  
-  // send: 전송
-  res.render('index', { title: '홈페이지', pageName: 'home.ejs'});
+  res.render('index', { title: '홈페이지', pageName: 'home.ejs' });
 });
 
 module.exports = router;
--- routes/locals.js
+++ routes/locals.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* 도서 검색 page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '도서검색', pageName: 'books/search.ejs' });
+  res.render('index', { title: '지역검색', pageName: 'locals/search.ejs' });
 });
 
 module.exports = router;
