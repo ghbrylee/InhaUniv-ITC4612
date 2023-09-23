1957cbaf081666c7e148d867e35af112e93c327f
Geonhwi Lee <bryanlee@blockware.co.kr>
2023-09-23 15:45:03 +0900

    4주차 수업

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* 도서검색 */
+// 도서 검색 페이지
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '도서검색', pageName:'books/search.ejs' });
+  res.render('index', { title: '도서검색', pageName: 'books/search.ejs' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '홈페이지', pageName:'home.ejs' });
+  // render: 페이지 출력
+  // res.render('index', { title: '익스프레스', name: '홍길동' });
+  
+  // send: 전송
+  res.render('index', { title: '홈페이지', pageName: 'home.ejs'});
 });
 
 module.exports = router;
